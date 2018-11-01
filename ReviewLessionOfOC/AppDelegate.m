ea893c66a25809eb14e303318c3bf2e594178a90
Dylan Xiao <dream9on@163.com>
2018-11-01 11:58:37 +0800

    Initial Commit

---
+++ ReviewLessionOfOC/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  ReviewLessionOfOC
+//
+//  Created by Dylan Xiao on 2018/11/1.
+//  Copyright © 2018年 Dylan Xiao. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
---
+++ ReviewLessionOfOC/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ReviewLessionOfOC
+//
+//  Created by Dylan Xiao on 2018/11/1.
+//  Copyright © 2018年 Dylan Xiao. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
